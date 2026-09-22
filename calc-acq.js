1abf0c396e29da3f119c5e2e116b0472a315a7fc
939tax-ops <939tax-ops@users.noreply.github.com>
2026-09-22 03:17:00 +0000

    취득세 계산기: 감면 적용 시 부가세목 계산 차이 가능 안내

--- calc-acq.js
+++ calc-acq.js
@@ -48,6 +48,7 @@
     $('tbl').innerHTML=t;
     var m=[];
     if(r.over12) m.push('<b style="color:#c62828">12억원 초과는 생애최초 감면 제외</b>');
+    if(r.red) m.push('감면 적용 시 지방교육세·농어촌특별세는 관할 지자체 계산과 차이 가능');
     m.push('원 단위 끝전 처리에 따라 실제 고지세액과 소액 차이 가능');
     m.push('등기 비용(국민주택채권 매입, 법무사 보수 등)은 제외');
     $('msg').innerHTML='· '+m.join('<br>· ');
